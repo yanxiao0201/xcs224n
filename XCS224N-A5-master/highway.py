a9adf0be620598d773d076a7053881be26808874
Xiao Yan <xyan@linkedin.com>
2019-11-17 21:02:31 -0800

    add assignment 5

---
+++ XCS224N-A5-master/highway.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+### YOUR CODE HERE for part 1d
+class Highway(nn.Module):
+
+    def __init__(self, embedding_size):
+        """
+        Init Highway model
+        :param embedding_size (int): Embedding size (dimensionality)
+        """
+        super(Highway, self).__init__()
+        self.projection = nn.Linear(embedding_size, embedding_size)
+        self.gate = nn.Linear(embedding_size, embedding_size)
+
+    def forward(self, x_conv_out):
+        """
+        Map from x_conv_out to x_highway
+        :param x_conv_out: Tensor output from cnn layer. Input size (batch_size, embedding_size)
+        :return: x_highway: Tensor output from Highway network. Output size (batch_size, embedding_size)
+        """
+        x_proj = F.relu(self.projection(x_conv_out))
+        x_gate = torch.sigmoid(self.gate(x_conv_out))
+
+        x_highway = x_gate * x_proj + (1 - x_gate) * x_conv_out
+        return x_highway
+### END YOUR CODE 
+
